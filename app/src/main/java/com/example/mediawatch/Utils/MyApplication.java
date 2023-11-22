58f7b90c1a4903af511c1a6e3a364340a82e8a36
unknown <tonymaina36@gmail.com>
2023-11-22 11:26:13 +0300

    Added Signleton MyApplication class to handle json request

---
+++ app/src/main/java/com/example/mediawatch/Utils/MyApplication.java
@@ -0,0 +1,27 @@
+package com.example.mediawatch.Utils;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MyApplication extends Application {
+
+    private static MyApplication instance;
+    private RequestQueue requestQueue;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+    }
+
+    public static synchronized MyApplication getInstance() {
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        return requestQueue;
+    }
+}
