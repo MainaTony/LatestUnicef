ba1e54e5ff323c9514c085a8248854f88c4c1862
unknown <tonymaina36@gmail.com>
2023-11-22 11:45:09 +0300

    Code Cleanup of the MyApplication class

--- app/src/main/java/com/example/mediawatch/LoginMain.java
+++ app/src/main/java/com/example/mediawatch/LoginMain.java
@@ -12,6 +12,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.mediawatch.Utils.JsonApiResponse;
+import com.example.mediawatch.Utils.JsonApiResponse.*;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,6 +25,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.mediawatch.ApiResponse.ApiResponse;
+import com.example.mediawatch.Utils.MyApplication;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -49,17 +53,15 @@ public class LoginMain extends AppCompatActivity {
         myLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(LoginMain.this, Verify.class);
-//                startActivity(intent);
-//                login();
+
+
                 try {
                     if(!email.getText().toString().isEmpty() && !password.getText().toString().isEmpty()){
                         JSONObject params = new JSONObject();
                         params.put("username", email.getText().toString());
                         params.put("password", password.getText().toString());
+                        JsonApiResponse.makeJsonRequest(url, params);
 
-//                    makeJsonRequest("\"http://192.168.100.111:8080/portal/auth/authenticate\"", params);
-                        makeJsonRequest(url, params);
                     } else{
                         Toast.makeText(LoginMain.this, "Please Enter Your Username or password", Toast.LENGTH_SHORT).show();
                     }
@@ -72,43 +74,7 @@ public class LoginMain extends AppCompatActivity {
         });
     }
 
-    public void makeJsonRequest(String url, final JSONObject params) {
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
 
-        // Request a JSON response from the provided URL.
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, params,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // Handle the JSON response
-                        ApiResponse responseModel = parseJsonResponse(response);
-                        Log.d(TAG, "My Response Code " + responseModel.getResponseCode());
-                        Log.d(TAG, "Description " + responseModel.getResponseDescription());
-
-                        if(responseModel.getResponseCode().equals("00")){
-                            Toast.makeText(LoginMain.this, "Login Success", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(LoginMain.this, MainActivity.class);
-                            startActivity(intent);
-                        } else if(responseModel.getResponseCode().equals("01")){
-                            Toast.makeText(LoginMain.this, responseModel.getResponseDescription(), Toast.LENGTH_SHORT).show();
-                            Log.d(TAG, "Description " + responseModel.getResponseDescription());
-                        }
-                        //
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Handle errors
-                        System.out.println("Error: " + error.toString());
-                    }
-                });
-
-        // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest);
-    }
 
 
 
--- app/src/main/java/com/example/mediawatch/Utils/JsonApiResponse.java
+++ app/src/main/java/com/example/mediawatch/Utils/JsonApiResponse.java
@@ -1,6 +1,18 @@
 package com.example.mediawatch.Utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.mediawatch.ApiResponse.ApiResponse;
+import com.example.mediawatch.LoginMain;
+import com.example.mediawatch.MainActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,14 +25,37 @@ public class JsonApiResponse {
             // Parse JSON and set values in your model class
             responseModel.setResponseCode(response.getString("responseCode"));
             responseModel.setResponseDescription(response.getString("responseDescription"));
-//            responseModel.setField2(response.getInt("field2"));
-            // Set more fields as needed
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
         return responseModel;
     }
 
+    public static void makeJsonRequest(String url, final JSONObject params) {
+        RequestQueue requestQueue = MyApplication.getInstance().getRequestQueue();
+
+        // Request a JSON response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Handle the JSON response
+                        ApiResponse responseModel = parseJsonResponse(response);
+                        Log.d(Constants.TAG, "My Response Code " + responseModel.getResponseCode());
+                        Log.d(Constants.TAG, "Description " + responseModel.getResponseDescription());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors
+                        System.out.println("Error: " + error.toString());
+                    }
+                });
+
+        // Add the request to the RequestQueue.
+        requestQueue.add(jsonObjectRequest);
+    }
 
 }
