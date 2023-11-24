1037f092440772c26ab3d41d6ccdbbc05b202676
unknown <tonymaina36@gmail.com>
2023-11-24 13:13:36 +0300

    added the News Activity screen

--- app/src/main/java/com/example/mediawatch/Home.java
+++ app/src/main/java/com/example/mediawatch/Home.java
@@ -2,11 +2,15 @@ package com.example.mediawatch;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,8 @@ public class Home extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private ArrayList<News> newsArrayList;
 
     public Home() {
         // Required empty public constructor
@@ -60,5 +66,15 @@ public class Home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState); 
+        dataInitialize();
+    }
+
+    private void dataInitialize() {
+        newsArrayList = new ArrayList<>();
     }
 }
