e13f2a611d48ca3b020d2beb920f72bd96a8df9e
unknown <tonymaina36@gmail.com>
2023-12-07 12:55:44 +0300

    Added the Group Chat screen

--- app/src/main/java/com/example/mediawatch/ChatCreateChannel.java
+++ app/src/main/java/com/example/mediawatch/ChatCreateChannel.java
@@ -3,12 +3,22 @@ package com.example.mediawatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ChatCreateChannel extends AppCompatActivity {
+    ImageView chat_create_channels_back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_create_channel);
+        chat_create_channels_back_button = findViewById(R.id.chat_create_channels_back_button);
+        chat_create_channels_back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mediawatch/ChatGroupChatScreen.java
@@ -0,0 +1,14 @@
+package com.example.mediawatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatGroupChatScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_group_chat_screen);
+    }
+}
